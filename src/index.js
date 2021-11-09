9eb62a864ce00b01ad2cfb1aacfc4a1b7145efd8
ariana <arianamallquer@gmail.com>
2021-11-08 22:38:11 -0500

    proyecto finalizado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,98 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+
+let usuario=window.prompt("Ingresa tu nombre");
+let msj= document.getElementById("nombreh2");
+
+    while(usuario==""){
+        alert("Ingresa tu nombre, por favor");
+        usuario = window.prompt("Ingresa tu nombre");
+    }
+        msj.innerHTML="Bienvenida/o "+usuario+" a nuestro vívero Gran Orquidea";
+
+
+
+let numeroTarjeta= document.getElementById("numero");
+
+//Quitar espacios y letras al input donde se coloca el número de la tarjeta
+
+let x=document.getElementById("numero");
+x.addEventListener("keyup", (e)=>{
+    let espacios = e.target.value;
+    x.value = espacios.replace(/\s/g, "").replace(/\D/g, "");
+});
+
+//boton "Validar Tarjeta"
+
+let boton= document.getElementById("validar");
+boton.addEventListener('click', ()=>{
+    let valorNumero = numeroTarjeta.value;
+    //console.log(validator.isValid(valorNumero));
+    
+    //let cuadro= document.getElementById("cuadro");
+    //cuadro.innerHTML= validator.isValid(valorNumero) + validator.maskify(valorNumero);
+    if(valorNumero==""){
+        alert("Por favor, ingresa el número de tu tarjeta");
+        return true;
+    }
+    
+    if (validator.isValid(valorNumero)==true){
+        document.getElementById("positivo").style.display="block";
+        document.getElementById("mensajePositivo").innerHTML= "¡Tú número de tarjeta " + validator.maskify(valorNumero) +" es válida! ¡Gracias por tu compra!";
+        document.getElementById("negativo").style.display="none";
+    }else{
+        document.getElementById("negativo").style.display="block";
+        document.getElementById("mensajeNegativo").innerHTML= "¡Uy, lo sentimos! Tú número de tarjeta " + validator.maskify(valorNumero) +" no es válida ): ";
+        document.getElementById("positivo").style.display="none";
+    }
+});
+
+//boton "Limpiar Datos"
+
+let boton2 = document.getElementById("btnReset");
+boton2.addEventListener('click',()=>{
+        document.getElementById("form1").reset();
+        document.getElementById("positivo").style.display="none";
+        document.getElementById("negativo").style.display="none";
+});
+
+//mostrar formulario con imagen
+
+let botonRosas = document.getElementById("planta1");
+botonRosas.addEventListener('click',()=>{muestraFormulario("Rosas")});
+let botonMargaritas = document.getElementById("planta2");
+botonMargaritas.addEventListener('click',()=>{muestraFormulario("Margaritas")});
+let botonNMO = document.getElementById("planta3");
+botonNMO.addEventListener('click',()=>{muestraFormulario("NMO")});
+
+function muestraFormulario(boton){
+        document.getElementById("banner").style.display="none";
+        document.getElementById("bienvenidos").style.display="none";
+        document.getElementById("vivero").style.display="none";
+        if(boton=="Rosas"){
+            document.getElementById("planta2").style.display="none";
+            document.getElementById("planta3").style.display="none";
+        } else if (boton=="Margaritas") {
+            document.getElementById("planta1").style.display="none";
+            document.getElementById("planta3").style.display="none";
+        } else {
+            document.getElementById("planta1").style.display="none";
+            document.getElementById("planta2").style.display="none";
+        }
+        document.getElementById("tarjeta").style.display="block";
+}
+
+let botonInicio = document.getElementById("inicio");
+botonInicio.addEventListener('click', ()=>{
+    document.getElementById("banner").style.display="block";
+    document.getElementById("bienvenidos").style.display="block";
+    document.getElementById("vivero").style.display="block";
+    document.getElementById("planta1").style.display="block";
+    document.getElementById("planta2").style.display="block";
+    document.getElementById("planta3").style.display="block";
+    document.getElementById("tarjeta").style.display="none";
+    document.getElementById("positivo").style.display="none";
+    document.getElementById("negativo").style.display="none";
+});
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,48 @@
 const validator = {
-  // ...
-};
+  // Algoritmo de Lunh
 
-export default validator;
+  isValid: function(valorNumero){
+    // Pasar los números a un array y en orden inverso
+    let numeroInverso = Array.from(valorNumero).reverse();
+    let sumatotal=0;
+    
+    // Ubicar las posiciones pares
+    for(let i = 0; i<numeroInverso.length; i++){
+      if((i+1) % 2 == 0){ // Para que mi posición empiece desde 1
+        let cifra = (numeroInverso[i]*2);// Multiplicar por 2 a las posiciones pares
+        if(cifra>=10){
+          let cifra1 = Math.floor(cifra/10);
+          let cifra2 = cifra % 10;
+          numeroInverso[i]= cifra1 + cifra2;
+        }
+        else{
+          numeroInverso[i] = cifra;
+        }
+      
+      }else{
+        numeroInverso[i] = numeroInverso[i]*1;
+      }
+      sumatotal = sumatotal + numeroInverso[i];
+    }
+    
+    if(sumatotal%10==0){
+      return true;
+    } else {
+      return false;
+    }
+    
+  },
+  
+  maskify: function(valorNumero){
+   let largoTarjeta = valorNumero.length - 4; //todos los numeros menos las ultimas 4 posiciones
+   let cuatroNumeros = valorNumero.substring(valorNumero.length - 4 ); // 4 ultimas posiciones
+
+   let resultado = "";
+
+   for(let mask = 1; mask <= largoTarjeta; mask++){ //oculto los numeros menos los ultimos 4
+    resultado += "#";
+  } //console.log(resultado + cuatroNumeros)
+  return resultado + cuatroNumeros; //sumo los numeros visibles y ocultos
+  }
+}
+ export default validator;
